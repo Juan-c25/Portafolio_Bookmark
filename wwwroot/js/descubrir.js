e2490c61a3671b3e988638e593c86f2c4f6baa82
jua.venegasc <juan.c.venegas.c@gmail.com>
2022-06-10 22:51:28 -0400

    cambios en el front, pq no logre hacer el guardar

---
+++ wwwroot/js/descubrir.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var item, tile, author, publisher, bookLink, bookImg;
+    var outputList = document.getElementById("list-output");
+    var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+    var apiKey = "Key=AIzaSyDcoLxcFGM6uiIgYdYe_HqO7EblZ2gkcI4";
+    var placeHldr = 'https://ctuid.com/img/not-found.png';
+    var searchData;
+
+})
